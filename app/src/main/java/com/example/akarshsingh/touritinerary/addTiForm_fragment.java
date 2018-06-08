11e272e5284b18c7d4dd789d3ce2333399cc43e2
akarshsingh9 <akarsh.singh9@gmail.com>
2018-06-08 15:05:27 +0530

    develop branch commit - fragments added

---
+++ app/src/main/java/com/example/akarshsingh/touritinerary/addTiForm_fragment.java
@@ -0,0 +1,29 @@
+package com.example.akarshsingh.touritinerary;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class addTiForm_fragment extends Fragment {
+
+
+    public addTiForm_fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_ti_form, container, false);
+    }
+
+}
