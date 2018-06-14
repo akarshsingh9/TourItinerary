9934161d00ebcefc537801705a40fa25b2d1341d
akarshsingh9 <akarsh.singh9@gmail.com>
2018-06-14 15:33:37 +0530

    addTI_Form Activity layout changed and updated according to the new field provided

--- app/src/main/java/com/example/akarshsingh/touritinerary/addTI_Form.java
+++ app/src/main/java/com/example/akarshsingh/touritinerary/addTI_Form.java
@@ -6,7 +6,9 @@ import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.INotificationSideChannel;
@@ -15,12 +17,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -36,10 +44,13 @@ import java.util.List;
 public class addTI_Form extends AppCompatActivity {
 
 // Views variables declared
-    Button setbtn,setbtn1;
-    EditText timedept,datedept,purposeEditText;
-    Spinner travelmode,fromspinner, tospinner;
-    FloatingActionButton submitfab;
+    Button submit;
+    RadioGroup radioGroup;
+    RadioButton oneWay, roundTrip;
+    Spinner travelModeSpinner, fromplaceSpinner, toplaceSpinner, prefTimeSpinner, prefDestinationSpinner;
+    EditText deptDate, reachDate, deptDestinationDate, reachDestinationDate, daysStayed, purpose, remarks;
+    ImageButton deptCal, reachCal, deptDestinationCal, reachDestinationCal;
+    String travelmodeSelected;
 
 
     @Override
@@ -50,119 +61,89 @@ public class addTI_Form extends AppCompatActivity {
         //ActionBar
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-//===========================================================================================================
-        // all views initialized
-        setbtn = (Button)findViewById(R.id.setbtn);
-        setbtn1 = (Button)findViewById(R.id.setbtn1);
-        timedept = (EditText)findViewById(R.id.timedept);
-        datedept = (EditText)findViewById(R.id.datedept);
-        travelmode = (Spinner)findViewById(R.id.travelmode_spinner);
-        fromspinner = (Spinner)findViewById(R.id.from_spinner);
-        tospinner = (Spinner)findViewById(R.id.to_spinner);
-        submitfab = (FloatingActionButton)findViewById(R.id.submit_fab);
-        purposeEditText = (EditText)findViewById(R.id.purpose);
-//======================================================================================================================
-        //travel mode spinner
-        List<String> travelmode_list = new ArrayList<String>();
-        travelmode_list.add("Rail");
-        travelmode_list.add("Flight");
+        //hide soft keyboard
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+//==============================================================================================================
+        //view initialization
+        submit = (Button)findViewById(R.id.submitFormBtn);
+        radioGroup = (RadioGroup)findViewById(R.id.tripRadioGroup);
+        oneWay = (RadioButton)findViewById(R.id.oneway);
+        roundTrip = (RadioButton)findViewById(R.id.roundtrip);
+        travelModeSpinner = (Spinner)findViewById(R.id.travelmodeSpinner);
+        fromplaceSpinner = (Spinner)findViewById(R.id.fromSpinner);
+        toplaceSpinner = (Spinner)findViewById(R.id.toSpinner);
+        prefTimeSpinner = (Spinner)findViewById(R.id.prefTime);
+        prefDestinationSpinner = (Spinner)findViewById(R.id.prefTime2);
+        deptDate = (EditText)findViewById(R.id.deptEditText);
+        deptDestinationDate = (EditText)findViewById(R.id.deptEditText2);
+        reachDate = (EditText)findViewById(R.id.reachEditText);
+        reachDestinationDate = (EditText)findViewById(R.id.reachEditText2);
+        daysStayed = (EditText)findViewById(R.id.stayEditText);
+        purpose = (EditText)findViewById(R.id.purposeEditText);
+        remarks = (EditText)findViewById(R.id.remarksEditText);
+        deptCal = (ImageButton)findViewById(R.id.deptDateImageButton);
+        deptDestinationCal = (ImageButton)findViewById(R.id.deptDateImageButton2);
+        reachCal = (ImageButton)findViewById(R.id.reachImageButton);
+        reachDestinationCal = (ImageButton)findViewById(R.id.reachImageButton2);
+//===================================================================================================================
+        //travelmode Spinner
+        List<String> travelMode_list = new ArrayList<>();
+        travelMode_list.add("Select a Travel Mode");
+        travelMode_list.add("Air");
+        travelMode_list.add("Rail");
+        travelMode_list.add("Own Car");
+        travelMode_list.add("Others");
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,travelmode_list);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        travelmode.setAdapter(arrayAdapter);
-//=======================================================================================================================
-        //from spinner
-        List<String> fromplaces = new ArrayList<String>();
-        fromplaces.add("Delhi");
-        fromplaces.add("Mumbai");
-        fromplaces.add("Ahmedabad");
-        fromplaces.add("Chennai");
-        ArrayAdapter<String> fromarrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,fromplaces);
-        fromarrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        fromspinner.setAdapter(fromarrayAdapter);
-//=======================================================================================================================
-        //To spinner
-        List<String> toplaces = new ArrayList<String>();
-        toplaces.add("Delhi");
-        toplaces.add("Mumbai");
-        toplaces.add("Ahmedabad");
-        toplaces.add("Chennai");
-        ArrayAdapter<String> toarrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,toplaces);
-        toarrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        tospinner.setAdapter(toarrayAdapter);
-//=======================================================================================================================
-        //date btn onclick listener
-
-        setbtn.setOnClickListener(new View.OnClickListener() {
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this,R.layout.spinner_item,travelMode_list)
+        {
             @Override
-            public void onClick(View v) {
-
-                final Calendar calendar = Calendar.getInstance();
-                int mYear = calendar.get(Calendar.YEAR);
-                int mMonth = calendar.get(calendar.MONTH);
-                int mDay = calendar.get(calendar.DAY_OF_MONTH);
-
-                DatePickerDialog datePickerDialog = new DatePickerDialog(addTI_Form.this, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        datedept.setText(""+dayOfMonth+"/"+""+(month+1)+"/"+""+year);
-                    }
-                },mYear,mMonth,mDay);
-
-                datePickerDialog.show();
-
-            }
-        });
-//============================================================================================================================
-        // time btn onclick listener
-
-        setbtn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (TextUtils.equals(travelmode.getSelectedItem().toString(),"Flight"))
+            public boolean isEnabled(int position) {
+                if (position==0)
                 {
-                    final Calendar calendar = Calendar.getInstance();
-                    int mMinute = calendar.get(calendar.MINUTE);
-                    int mHour = calendar.get(calendar.HOUR_OF_DAY);
-
-                    TimePickerDialog timePickerDialog = new TimePickerDialog(addTI_Form.this, new TimePickerDialog.OnTimeSetListener() {
-                        @Override
-                        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                            timedept.setText(""+hourOfDay+":"+""+minute);
-                        }
-                    },mHour,mMinute,false);
-
-                    timePickerDialog.show();
-
+                    return false;
                 }
                 else
                 {
-                    Toast.makeText(addTI_Form.this,"Applicable only on Flight",Toast.LENGTH_SHORT).show();
+                    return true;
                 }
-                            }
-
-        });
-//===============================================================================================================================
-        // submit fab onclick listener
-
-        submitfab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(addTI_Form.this,addTI.class);
-                intent.putExtra("flag",1);
-                intent.putExtra("from",fromspinner.getSelectedItem().toString());
-                intent.putExtra("to",tospinner.getSelectedItem().toString());
-                intent.putExtra("travelmode",travelmode.getSelectedItem().toString());
-                intent.putExtra("date",datedept.getText().toString());
-                intent.putExtra("time",timedept.getText().toString());
-                intent.putExtra("purpose",purposeEditText.getText().toString());
-
-
-                startActivity(intent);
             }
 
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView textView = (TextView)view;
+                if (position==0)
+                {
+                    textView.setTextColor(Color.GRAY);
+                }
+                else
+                {
+                    textView.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+
+
+        };
+        spinnerAdapter.setDropDownViewResource(R.layout.spinner_item);
+        travelModeSpinner.setAdapter(spinnerAdapter);
+
+        travelModeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position>0)
+                {
+                    travelmodeSelected = (String)parent.getItemAtPosition(position);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
+//=========================================================================================================================
+
 
 //=============================================================================================================================
     } //end of onCreate()
