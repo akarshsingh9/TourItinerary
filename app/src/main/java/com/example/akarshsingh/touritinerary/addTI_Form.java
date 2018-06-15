4db5221bbbbdf27be069c3a4f09ac2a2224cfbb6
akarshsingh9 <akarsh.singh9@gmail.com>
2018-06-15 09:08:23 +0530

    addTIForm spinners and calendar added

--- app/src/main/java/com/example/akarshsingh/touritinerary/addTI_Form.java
+++ app/src/main/java/com/example/akarshsingh/touritinerary/addTI_Form.java
@@ -50,7 +50,8 @@ public class addTI_Form extends AppCompatActivity {
     Spinner travelModeSpinner, fromplaceSpinner, toplaceSpinner, prefTimeSpinner, prefDestinationSpinner;
     EditText deptDate, reachDate, deptDestinationDate, reachDestinationDate, daysStayed, purpose, remarks;
     ImageButton deptCal, reachCal, deptDestinationCal, reachDestinationCal;
-    String travelmodeSelected;
+    TextView deptTV,deptTV2,reachTV,reachTV2,prefTV,prefTV2;
+    String travelmodeSelected,fromplaceSelected,toplaceSelected, prefTimeSelected, prefDestinationSelected;
 
 
     @Override
@@ -85,6 +86,12 @@ public class addTI_Form extends AppCompatActivity {
         deptDestinationCal = (ImageButton)findViewById(R.id.deptDateImageButton2);
         reachCal = (ImageButton)findViewById(R.id.reachImageButton);
         reachDestinationCal = (ImageButton)findViewById(R.id.reachImageButton2);
+        deptTV = (TextView)findViewById(R.id.deptText);
+        deptTV2 = (TextView)findViewById(R.id.deptText2);
+        reachTV = (TextView)findViewById(R.id.reachingText);
+        reachTV2 = (TextView)findViewById(R.id.reachingText2);
+        prefTV = (TextView)findViewById(R.id.prefText);
+        prefTV2 = (TextView)findViewById(R.id.prefText2);
 //===================================================================================================================
         //travelmode Spinner
         List<String> travelMode_list = new ArrayList<>();
@@ -143,9 +150,369 @@ public class addTI_Form extends AppCompatActivity {
             }
         });
 //=========================================================================================================================
+        //from spinner
+        List<String> fromlist = new ArrayList<String>();
+        fromlist.add("Select a City");
+        fromlist.add("Ahmedabad");
+        fromlist.add("Bangalore");
+        fromlist.add("Chennai");
+        fromlist.add("Delhi");
+        fromlist.add("Kolkata");
+        fromlist.add("Mumbai");
+        fromlist.add("Patna");
+        fromlist.add("Ranchi");
 
 
+        ArrayAdapter<String> fromAdapter = new ArrayAdapter<String>(this,R.layout.spinner_item,fromlist)
+        {
+            @Override
+            public boolean isEnabled(int position) {
+                if (position==0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView textView = (TextView)view;
+                if (position==0)
+                {
+                    textView.setTextColor(Color.GRAY);
+                }
+                else
+                {
+                    textView.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+
+
+        };
+        fromAdapter.setDropDownViewResource(R.layout.spinner_item);
+        fromplaceSpinner.setAdapter(fromAdapter);
+
+        fromplaceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position>0)
+                {
+                    fromplaceSelected = (String)parent.getItemAtPosition(position);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+//===========================================================================================================================
+        //to spinner
+        List<String> tolist = new ArrayList<String>();
+        fromlist.add("Select a City");
+        fromlist.add("Ahmedabad");
+        fromlist.add("Bangalore");
+        fromlist.add("Chennai");
+        fromlist.add("Delhi");
+        fromlist.add("Kolkata");
+        fromlist.add("Mumbai");
+        fromlist.add("Patna");
+        fromlist.add("Ranchi");
+
+
+        ArrayAdapter<String> toAdapter = new ArrayAdapter<String>(this,R.layout.spinner_item,tolist)
+        {
+            @Override
+            public boolean isEnabled(int position) {
+                if (position==0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView textView = (TextView)view;
+                if (position==0)
+                {
+                    textView.setTextColor(Color.GRAY);
+                }
+                else
+                {
+                    textView.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+
+
+        };
+        toAdapter.setDropDownViewResource(R.layout.spinner_item);
+        toplaceSpinner.setAdapter(fromAdapter);
+
+        toplaceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position>0)
+                {
+                    toplaceSelected = (String)parent.getItemAtPosition(position);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+ //========================================================================================================================
+        //pref time spinner
+        List<String> prefList = new ArrayList<String>();
+        prefList.add("Select an option");
+        prefList.add("0-1");
+        prefList.add("1-2");
+        prefList.add("2-3");
+        prefList.add("3-4");
+        prefList.add("5-6");
+        prefList.add("6-7");
+        prefList.add("7-8");
+        prefList.add("8-9");
+        prefList.add("9-10");
+        prefList.add("10-11");
+        prefList.add("11-12");
+        prefList.add("12-13");
+        prefList.add("13-14");
+        prefList.add("14-15");
+        prefList.add("15-16");
+        prefList.add("16-17");
+        prefList.add("17-18");
+        prefList.add("18-19");
+        prefList.add("19-20");
+        prefList.add("20-21");
+        prefList.add("21-22");
+        prefList.add("22-23");
+        prefList.add("23-24");
+
+        ArrayAdapter<String> timeAdapter = new ArrayAdapter<String>(this,R.layout.spinner_item,prefList)
+        {
+            @Override
+            public boolean isEnabled(int position) {
+                if (position==0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView textView = (TextView)view;
+                if (position==0)
+                {
+                    textView.setTextColor(Color.GRAY);
+                }
+                else
+                {
+                    textView.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+
+
+        };
+        timeAdapter.setDropDownViewResource(R.layout.spinner_item);
+        prefTimeSpinner.setAdapter(timeAdapter);
+
+        prefTimeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position>0)
+                {
+                    prefTimeSelected = (String)parent.getItemAtPosition(position);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+//==========================================================================================================================
+        //pref destination spinner
+        ArrayAdapter<String> preftimeAdapter = new ArrayAdapter<String>(this,R.layout.spinner_item,prefList)
+        {
+            @Override
+            public boolean isEnabled(int position) {
+                if (position==0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView textView = (TextView)view;
+                if (position==0)
+                {
+                    textView.setTextColor(Color.GRAY);
+                }
+                else
+                {
+                    textView.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+
+
+        };
+        preftimeAdapter.setDropDownViewResource(R.layout.spinner_item);
+        prefDestinationSpinner.setAdapter(preftimeAdapter);
+
+        prefDestinationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position>0)
+                {
+                    prefDestinationSelected = (String)parent.getItemAtPosition(position);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+//===========================================================================================================================
+        //dept date Calendar
+        deptCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int mYear = calendar.get(Calendar.YEAR);
+                int mMonth = calendar.get(calendar.MONTH);
+                int mDay = calendar.get(calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(addTI_Form.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        deptDate.setText(""+dayOfMonth+"/"+""+(month+1)+"/"+""+year);
+                    }
+                },mYear,mMonth,mDay);
+
+                datePickerDialog.show();
+            }
+        });
 //=============================================================================================================================
+        //reaching date Calendar
+        reachCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int mYear = calendar.get(Calendar.YEAR);
+                int mMonth = calendar.get(calendar.MONTH);
+                int mDay = calendar.get(calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(addTI_Form.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        reachDate.setText(""+dayOfMonth+"/"+""+(month+1)+"/"+""+year);
+                    }
+                },mYear,mMonth,mDay);
+
+                datePickerDialog.show();
+            }
+        });
+//=============================================================================================================================
+        //dept date destination calendar
+        deptDestinationCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int mYear = calendar.get(Calendar.YEAR);
+                int mMonth = calendar.get(calendar.MONTH);
+                int mDay = calendar.get(calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(addTI_Form.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        deptDestinationDate.setText(""+dayOfMonth+"/"+""+(month+1)+"/"+""+year);
+                    }
+                },mYear,mMonth,mDay);
+
+                datePickerDialog.show();
+            }
+        });
+//===========================================================================================================================
+        //reach date destination calendar
+        reachDestinationCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int mYear = calendar.get(Calendar.YEAR);
+                int mMonth = calendar.get(calendar.MONTH);
+                int mDay = calendar.get(calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(addTI_Form.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        reachDestinationDate.setText(""+dayOfMonth+"/"+""+(month+1)+"/"+""+year);
+                    }
+                },mYear,mMonth,mDay);
+
+                datePickerDialog.show();
+            }
+        });
+//=============================================================================================================================
+        //RadioButton Logic
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId)
+                {
+                    case R.id.oneway:
+                        deptTV2.setVisibility(View.GONE);
+                        deptDestinationDate.setVisibility(View.GONE);
+                        deptDestinationCal.setVisibility(View.GONE);
+                        reachTV2.setVisibility(View.GONE);
+                        reachDestinationCal.setVisibility(View.GONE);
+                        reachDestinationDate.setVisibility(View.GONE);
+                        /*
+                        if (travelModeSpinner.getSelectedItem().toString() == "Air")
+                        {
+                            prefTV2.setVisibility(View.GONE);
+                            prefDestinationSpinner.setVisibility(View.GONE);
+                        }
+                        else
+                        {
+                            prefTV.setVisibility(View.GONE);
+                            prefTimeSpinner.setVisibility(View.GONE);
+                            prefTV2.setVisibility(View.GONE);
+                            prefDestinationSpinner.setVisibility(View.GONE);
+                        }*/
+                        break;
+                    case R.id.roundtrip:
+                        break;
+
+                }
+            }
+        });
     } //end of onCreate()
 //==========================================================================================================================
     @Override
