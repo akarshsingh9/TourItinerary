9f21dd7657775d8246c34be1d6518dd5cd3cd2c6
akarshsingh9 <akarsh.singh9@gmail.com>
2018-06-20 12:39:23 +0530

    Create TI retrieves data from JSON created using emp.jsp file

--- app/src/main/java/com/example/akarshsingh/touritinerary/CreateTI.java
+++ app/src/main/java/com/example/akarshsingh/touritinerary/CreateTI.java
@@ -3,11 +3,15 @@ package com.example.akarshsingh.touritinerary;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,6 +20,14 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -50,7 +62,7 @@ public class CreateTI extends AppCompatActivity {
         final TextView status = (TextView)findViewById(R.id.status);
 
         //reading JSON and putting values in Views
-        tino.setText(tinum());
+        tino.setText("2016_123123");
 //==================================================================================================================
        //ActionBar
         ActionBar actionBar = getSupportActionBar();
@@ -60,31 +72,113 @@ public class CreateTI extends AppCompatActivity {
 //==================================================================================================================
         // Spinner enterBy
         final Spinner enterby_spinner = (Spinner)findViewById(R.id.enterby_spinner);
-        List<String> name_list = new ArrayList<String>();
+        final List<String> name_list = new ArrayList<String>();
+        name_list.add("Select a name");
+        final List<String> authName = new ArrayList<>();
+        final List<String> empName = new ArrayList<>();
+        final List<String> deptName = new ArrayList<>();
+        final List<String> tiForName = new ArrayList<>();
         //JSON read names added here
-        for(int i=0;i<3;i++)
-        {
-            name_list.add(enterby(i));
-        }
+        String url ="http://10.0.2.2:8080/emp.jsp";
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,name_list);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Volley used to retrieve JSON
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        JsonArrayRequest arrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    String officer_name = "";
+                    String empname = "";
+                    String empno = "";
+                    String empgrade = "";
+                    String tifor = "";
+                    String deptname = "";
+                    String auth = "";
+                    //JSONArray arr = response.getJSONArray("array");
+                    for (int index=0;index<response.length();index++)
+                    {
+                        JSONObject arr_obj = response.getJSONObject(index);
+                        empname = arr_obj.getString("emp_name");
+                        empName.add(empname);
+                        empno = arr_obj.getString("emp_no");
+                        officer_name = empname+" ("+empno+")";
+                        name_list.add(officer_name);
+
+                        empgrade = arr_obj.getString("emp_grade");
+                        tifor = officer_name + " - "+empgrade;
+                        tiForName.add(tifor);
+                        deptname = arr_obj.getString("emp_bu") + " - " +arr_obj.getString("emp_budesc");
+                        deptName.add(deptname);
+                        auth = arr_obj.getString("ro_name") + " ("+arr_obj.getString("ro_grade") +")";
+                        authName.add(auth);
+
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(arrayRequest);
+
+
+        // from array adapter
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.spinner_item,name_list)
+        {
+            @Override
+            public boolean isEnabled(int position) {
+                if (position==0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView textView = (TextView)view;
+                if (position==0)
+                {
+                    textView.setTextColor(Color.GRAY);
+                }
+                else
+                {
+                    textView.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+
+
+        };
+        arrayAdapter.setDropDownViewResource(R.layout.spinner_item);
         enterby_spinner.setAdapter(arrayAdapter);
+
+
         // Spinner item selected, text change in TextViews
         enterby_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                //Approving Authority Textview change
-                String name = String.valueOf(approveAuth(position));
-                approvename.setText(name);
-                //TI Created for Texteview change
-                String nameSelected = enterby_spinner.getSelectedItem().toString()+tiforExtract(position);
-                tifor.setText(nameSelected);
-                //preferred name TextView change
-                String pref_name = String.valueOf(prefname(position));
-                prefname.setText(pref_name);
-               //dept name Textview change
-               dept.setText(deptname(position));
+                int pos = position - 1;
+                if (pos>=0) {
+                    //Approving Authority Textview change
+                    approvename.setText(authName.get(pos));
+                    //TI Created for Texteview change
+                    tifor.setText(tiForName.get(pos));
+                    //preferred name TextView change
+                    prefname.setText(empName.get(pos));
+                    //dept name Textview change
+                    dept.setText(deptName.get(pos));
+                }
             }
 
             @Override
@@ -92,15 +186,20 @@ public class CreateTI extends AppCompatActivity {
 
             }
         });
+
 //========================================================================================================================
         //Spinner Travel type
         Spinner travelby = (Spinner)findViewById(R.id.traveltype_spinner);
         List<String> travel_list = new ArrayList<String>();
+        travel_list.add("Travel for Meeting");
+        travel_list.add("Travel for Training");
+        travel_list.add("Other Reason");
         //JSON read travel type added
+        /*
         for(int j=0;j<3;j++)
         {
             travel_list.add(traveltype(j));
-        }
+        }*/
         ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,travel_list);
         arrayAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         travelby.setAdapter(arrayAdapter1);
@@ -156,22 +255,7 @@ public class CreateTI extends AppCompatActivity {
 //==================================================================================================
 
     //TODO: remove loadJSON() therefore make changes in JSON as well
-    String loadJSON()
-    {
-        String json = null;
-        try {
-            InputStream is = getAssets().open("data.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-    return json;
-}
+
     String loadJSONfromAssets()
     {
         String json = null;
@@ -188,46 +272,7 @@ public class CreateTI extends AppCompatActivity {
         }
         return json;
     }
-//===================================================================================================
-    // retrieve TI number from JSON
-    String tinum()
-    {
-        String num = "";
-        try{
-            JSONObject obj = new JSONObject(loadJSON());
-            JSONObject arr = obj.getJSONObject("array");
-            JSONArray tino = arr.getJSONArray("ti_no");
-            num = tino.getString(0);
-            }
-        catch (JSONException e)
-        {
-            e.printStackTrace();
-        }
 
-        return num;
-    }
-//==================================================================================================
-    // retrieve enterBy name from JSON
-    String enterby(int index)
-    {
-        String officer_name = "";
-        String empname = "";
-        String empno = "";
-        try{
-            JSONObject object = new JSONObject(loadJSONfromAssets());
-            JSONArray arr = object.getJSONArray("array");
-            JSONObject arr_obj = arr.getJSONObject(index);
-            empname = arr_obj.getString("emp_name");
-            empno = arr_obj.getString("emp_no");
-            officer_name = empname+" ("+empno+")";
-        }
-        catch (JSONException e)
-        {
-            e.printStackTrace();
-        }
-
-        return officer_name;
-    }
 //===================================================================================================
     //retrieve name of the officer whose TI is created from JSON
     String tiforExtract(int index)
@@ -292,23 +337,7 @@ public class CreateTI extends AppCompatActivity {
         return final_deptname;
     }
 
-//===================================================================================================
 
-    // retrieve travel type from JSON
-    String traveltype(int index)
-    {
-        String type = "";
-        JSONObject object = null;
-        try {
-            object = new JSONObject(loadJSON());
-            JSONObject arr = object.getJSONObject("array");
-            JSONArray traveltype = arr.getJSONArray("traveltype");
-            type = traveltype.getString(index);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return type;
-    }
 //==================================================================================================
     // retrieve RO name from JSON as approving authority
     String approveAuth(int index)
